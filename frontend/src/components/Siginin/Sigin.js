a0c29122859dbc7c918fc2e865bd7e9a39cd07b0
soyyyyy <da5868@naver.com>
2024-05-01 19:41:13 +0900

    Stlye.login페이지 스타일 파일 분리

--- frontend/src/components/Siginin/Sigin.js
+++ frontend/src/components/Siginin/Sigin.js
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 export function SigninPage() {
 
     return (
-        <>
-            <div>회원가입 페이지</div>
-        </>
-    );
+        <SigninRootWrapper>
+            회원가입 페이지
+        </SigninRootWrapper>
+    )
 }
+
+const SigninRootWrapper = styled.div`
+	min-height: 100vh;
+	background-color: white;
+    display: flex;
+    flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
