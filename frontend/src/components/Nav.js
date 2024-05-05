a70bccfaed002a51c8983766de6baaaf5a2ba405
ssxbin <sssoobins@gmail.com>
2024-05-06 03:49:25 +0900

    Rename: nav와 footer 파일 이름 변경

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,4 +1,4 @@
-import './Nav.css'
+import s from'./Nav.module.css'
 import {Link} from "react-router-dom"
 import hadogIog from '../assets/HADOG.png'
 import defaultImage from '../assets/defaultImage.png'
@@ -8,23 +8,25 @@ function Nav() {
     const [isLogin, setIsLogin] = useState(true);
 
     return (
-        <div className="navbar">
-            <div className="logo">
+        <div className={s.navbar}>
+            <div className={s.logo}>
                 <Link to={"./"}>
-                    <img className="logo" alt="hadog_logo" src={hadogIog} />
+                    <img className={s.logo}alt="hadog_logo" src={hadogIog} />
                 </Link>
             </div>
-            <div className='pageLink'>
+            <div className={s.pageLink}>
                 <div>
                     <Link>
                         <strong>이름찾기</strong>
                     </Link>
                 </div>
-                <div className='dropmenu'>
+                <div className={s.dropmenu}>
                         <strong>입양하기</strong>
-                        <div className='dropContent'>
+                        <div className={s.dropContent}>
                             <Link><h4>보호소 입양 공고</h4></Link>
+                            <hr />
                             <Link><h4>임시 보호 입양 공고</h4></Link>
+                            <hr />
                             <Link><h4>입양 후기</h4></Link>
                         </div>
                 </div>
@@ -40,24 +42,25 @@ function Nav() {
                 </div>
             </div>
             
-            <div className='login'>
+            <div className={s.login}>
                 {isLogin ? (
-                    <div className='dropmenu'>
-                        <Link>
+                    <div className={s.dropmenu}>
                             <strong>유저이름</strong>
-                            <div className='dropContent'>
+                            <div className={s.dropContent}>
                                 <Link><h4>마이페이지</h4></Link>
+                                <hr />
                                 <Link><h4>로그아웃</h4></Link>
                             </div>
-                        </Link>
+                            <img className={s.profileImg} alt='profileImg' src={defaultImage}/>
                     </div>
                 ) : (
                     <Link>
                         <strong>로그인</strong>
+                        <img className={s.profileImg} alt='profileImg' src={defaultImage}/>
                     </Link>
                 )}
                 <Link>
-                    <img className='profileImg' alt='profileImg' src={defaultImage}/>
+                    
                 </Link>
             </div>
         </div>
