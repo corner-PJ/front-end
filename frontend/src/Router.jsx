079b724186522a7a72b5936df6f7d81ab539d1ca
soyyyyy <da5868@naver.com>
2024-05-23 01:38:17 +0900

    비밀번호 변경 페이지(연결 전)

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -2,6 +2,8 @@ import {  Route, Routes } from 'react-router-dom';
 
 import Signin from './pages/Siginin';
 import Login from './pages/Login';
+import Mypages from './pages/Mypage';
+import { PasswordChange } from './components/Mypage/PasswordChange';
 
 function AppNavigation() {
   return (
@@ -9,6 +11,8 @@ function AppNavigation() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signin" element={<Signin />} />  
+          <Route path="/mypage" element={<Mypages />} />  
+          <Route path="/mypage/passwordChange" element={<PasswordChange />} />
         </Routes>
     </>
   );
