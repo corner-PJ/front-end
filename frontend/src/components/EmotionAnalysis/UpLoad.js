fd82979ca28c83b8d2c42f8f8942acacaf52b3d6
ssxbin <sssoobins@gmail.com>
2024-05-30 02:55:32 +0900

    Feat: 감정추적 페이지

---
+++ frontend/src/components/EmotionAnalysis/UpLoad.js
@@ -0,0 +1,125 @@
+import styled from 'styled-components';
+import { useState, useRef } from 'react';
+import UpLoadIcon from '../../assets/UpLoadIcon.png';
+import { useNavigate } from 'react-router-dom';
+
+function UpLoad() {
+    const [video, setVideo] = useState(null);
+    const fileInputRef = useRef(null);
+
+    const navigate = useNavigate();
+
+    const goToResult = () => {
+        navigate(`/result/`);
+    }
+
+    const videoUpload = e => {
+        const selectedVideo = e.target.files[0];
+        if (selectedVideo) {
+            setVideo(URL.createObjectURL(selectedVideo));
+        }
+    };
+
+    return (
+        <UpLoadContainer>
+            <Title>반려견의 모습이 담긴 영상을 첨부해주세요</Title>
+            <VideoContainer >
+                {video ? 
+                ( <Video controls src={video} /> ) :
+                (
+                    <FileContainer>
+                        <FileIcon src={UpLoadIcon} alt="Upload Icon" />
+                        <PlaceholderText>동영상을 첨부해주세요.</PlaceholderText>
+                    </FileContainer>
+                )}
+            </VideoContainer>
+                <UpLoadVideo 
+                    type="file" 
+                    accept="video/*" 
+                    onChange={videoUpload}
+                    ref={fileInputRef}
+                />
+            
+            <StartButton onClick={() => goToResult()}>감정추적하기</StartButton>
+        </UpLoadContainer>
+    );
+}
+
+const UpLoadContainer = styled.div`
+    margin: 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.div`
+    font-size: 2em;
+    font-weight: bold;
+    margin-bottom: 100px;
+`;
+
+const VideoContainer = styled.div`
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 50px;
+    width: 900px;
+    height: 400px;
+    border: 2px dashed #000000;
+
+    &:before {
+        content: "";
+        position: absolute;
+        top: 30%; 
+        left: 18%; 
+        right: 18%; 
+        bottom: 17%; 
+        border-radius: 20px;
+        background: #FFE6E6;
+        z-index: -1;
+    }
+`;
+
+const Video = styled.video`
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
+`;
+
+const UpLoadVideo = styled.input`
+`;
+
+const FileContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #000;
+`;
+
+const FileIcon = styled.img`
+    width: 85px;
+    height: 100px;
+    margin-bottom: 50px;
+`;
+
+const PlaceholderText = styled.div`
+    font-size: 1.3em;
+    font-weight: bold;
+`;
+
+const StartButton = styled.button`
+    display: block;
+    margin: 100px auto;
+    padding: 22px 80px;
+    font-size: 25px;
+    font-weight: bold;
+    background-color: #FFE6E6;
+    border: none;
+    border-radius: 15px;
+    box-shadow: 0 5px 10px 0px rgba(0, 0, 0, 0.3);
+`
+
+export default UpLoad;
---
+++ frontend/src/pages/AnalysisResult.js
@@ -0,0 +1,9 @@
+import AnalysisResultPage from "../components/EmotionAnalysis/AnalysisResult";
+
+export default function AnalysisResult() {
+    return (
+    <>
+        <AnalysisResultPage />
+    </>
+    );
+}
