f7d2afe98a590a7e2cf72959201d072884fec6a1
soyyyyy <da5868@naver.com>
2024-06-08 23:34:05 +0900

    feat.삭제, 등록 모달창

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -3,6 +3,7 @@ import {  Route, Routes } from 'react-router-dom';
 import Signin from './pages/Siginin';
 import Login from './pages/Login';
 import Diary from './pages/Diary';
+import DiaryList from './pages/DiaryList';
 import EmotionTest from './pages/EmotionTest';
 
 function AppNavigation() {
@@ -12,15 +13,12 @@ function AppNavigation() {
           <Route path="/login" element={<Login />} />
           <Route path="/signin" element={<Signin />} />  
           <Route path="/diary" element={<Diary />} />  
-          
-
+          <Route path="/diary/list" element={<DiaryList />} />
           <Route path="/emotionTestUrl" element={<EmotionTest />} />    
         </Routes>
     </>
   );
 }
-
-// <Route path="/diary/diarylist" element={<Diary />} />
 
 
 export default function Router() {
