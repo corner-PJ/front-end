718ff4d183fa36f078c4a75a7ea7112deaada9c4
ssxbin <sssoobins@gmail.com>
2024-06-29 03:27:17 +0900

    router 수정 & 패키지 다운

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -3,11 +3,9 @@ import {  Route, Routes } from 'react-router-dom';
 import Signin from './pages/Signin';
 import Login from './pages/Login';
 import HomePage from './pages/Home';
-
 import SpeechSynthesis from './pages/SpeechSynthesis';
 import SpeechSynthesis2 from './pages/SpeechSynthesis2';
 import SpeechSynthesisResult from './pages/SpeechSynthesisResult';
-
 import Diary from './pages/Diary';
 import DiaryList from './pages/DiaryList';
 import EmotionTest from './pages/EmotionTest';
@@ -17,7 +15,16 @@ import Mypages from './pages/Mypage';
 import { PasswordChange } from './components/Mypage/PasswordChange';
 import MypetRegister from './pages/MypageRegister';
 import EmotionHistory from './pages/EmotionHistory';
-
+import Write from './pages/Write';
+import List from './pages/List';
+import ListDetail from './pages/ListDetail';
+import EmotionAnalysis from './pages/EmotionAnalysis';
+import SelectDog from './pages/SelectDog';
+import UpLoad from './pages/UpLoad';
+import AnalysisResult from './pages/AnalysisResult';
+import Review from './pages/Review';
+import ReviewWrite from './pages/ReviewWrite'
+import ReviewDetail from './pages/ReviewDetail';
 
 function AppNavigation() {
   return (
@@ -38,6 +45,16 @@ function AppNavigation() {
           <Route path="/mypage/passwordChange" element={<PasswordChange />} />
           <Route path="/mypage/petRegister" element={<MypetRegister />} />  
           <Route path="/mypage/emotionHistory" element={<EmotionHistory />} />
+          <Route path="/write" element={<Write />} />  
+          <Route path="/list" element={<List />} />  
+          <Route path="/listDetail/:listId" element={<ListDetail />} />    
+          <Route path="/emotionAnalysis" element={<EmotionAnalysis />} />  
+          <Route path="/analysis" element={<SelectDog />} />  
+          <Route path="/analysis/:selectedDog" element={<UpLoad />} />
+          <Route path="/result/" element={<AnalysisResult />} />  
+          <Route path="/review" element={<Review />} />  
+          <Route path="/reviewWrite" element={<ReviewWrite />} />  
+          <Route path="/reviewDetail/:listId" element={<ReviewDetail />} /> 
         </Routes>
     </>
   );
