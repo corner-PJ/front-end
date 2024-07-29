045cbf1ce6bd4c809575bcebdd01bc970b2519fc
soyyyyy <da5868@naver.com>
2024-07-30 02:33:24 +0900

    style. router 공백 추가

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -31,29 +31,36 @@ function AppNavigation() {
   return (
     <>
         <Routes>
+          <Route path="/" element={<HomePage />} /> 
           <Route path="/login" element={<Login />} />
           <Route path="/signin" element={<Signin />} />  
-          <Route path="/" element={<HomePage />} />  
+        
           <Route path="/speechSynthesis" element={<SpeechSynthesis />} />  
           <Route path="/speechSynthesis2" element={<SpeechSynthesis2 />} />
           <Route path="/speechSynthesis/result" element={<SpeechSynthesisResult />} /> 
+          
           <Route path="/diary" element={<Diary />} />  
           <Route path="/diary/list" element={<DiaryList />} />
           <Route path="/diary/detail" element={<DiaryDetail />} />
           <Route path="/diary/new" element={<DiaryWrite />} />
+          
           <Route path="/emotionTestUrl" element={<EmotionTest />} />    
+          
           <Route path="/mypage" element={<Mypages />} />  
           <Route path="/mypage/passwordChange" element={<PasswordChange />} />
           <Route path="/mypage/petRegister" element={<MypetRegister />} />
           <Route path="/mypage/petEdit" element={<MypetEditPage /> }/>
           <Route path="/mypage/emotionHistory" element={<EmotionHistory />} />
+          
           <Route path="/write" element={<Write />} />  
           <Route path="/list" element={<List />} />  
           <Route path="/listDetail/:listId" element={<ListDetail />} />    
+          
           <Route path="/emotionAnalysis" element={<EmotionAnalysis />} />  
           <Route path="/analysis" element={<SelectDog />} />  
           <Route path="/analysis/:selectedDog" element={<UpLoad />} />
           <Route path="/result/" element={<AnalysisResult />} />  
+          
           <Route path="/review" element={<Review />} />  
           <Route path="/reviewWrite" element={<ReviewWrite />} />  
           <Route path="/reviewDetail/:listId" element={<ReviewDetail />} /> 
