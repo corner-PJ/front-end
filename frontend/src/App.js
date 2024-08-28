d5ca8b60cbf28f4fe57b93b41b737a5bca33dc36
ssxbin <sssoobins@gmail.com>
2024-08-29 03:22:57 +0900

    Fix: 공고 상세 id값 추가, Context데이터 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,13 @@ import './App.css';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import Router from './Router';
-import { ReviewProvider } from './components/ReviewContext'; 
-import { ListProvider } from './components/ListContext'; 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
-    <ReviewProvider>
-      <ListProvider>
+    <>
         <ToastContainer />
         <div className="App">
           <Nav />
@@ -20,8 +17,7 @@ function App() {
           </div>
           <Footer />
         </div>
-      </ListProvider>
-    </ReviewProvider>
+    </>
   );
 }
 
